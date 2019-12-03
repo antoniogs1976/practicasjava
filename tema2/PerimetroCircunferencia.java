4fe74f3397036e39112fb1c05ef5d12540cad89a
antoniogs1976 <antoniogs1976@gmail.com>
2019-12-03 17:48:10 +0100

    Cambios linux

--- tema2/PerimetroCircunferencia.java
+++ tema2/PerimetroCircunferencia.java
@@ -9,9 +9,10 @@ public class PerimetroCircunferencia {
         final double PI = 3.1415926536;
         double radio = 25.0, perimetro;
         perimetro = 2.0 * PI * radio;
-        System.out.print("El perimetro de la circunferencia de radio ");
+        System.out.print("----------\nEl perimetro de la circunferencia de radio ");
         System.out.print(radio);
         System.out.print(" es ");
         System.out.print(perimetro);
+        System.out.print("\n----------\n");
     } // Fin main
 } // Fin PerimetroCircunferencia
