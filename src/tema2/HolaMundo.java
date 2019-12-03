61285063010eb674eccf7b89e744b39a7a4f5c46
antoniogs1976 <58177868+antoniogs1976@users.noreply.github.com>
2019-12-03 13:01:00 +0100

    primera subida

---
+++ src/tema2/HolaMundo.java
@@ -0,0 +1,12 @@
+package tema2;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¡¡¡Hola Mundo, desde Eclipse!!!");
+		System.out.println("Dolores de cabeza JaVa S.A.");
+		
+	}
+	
+}
