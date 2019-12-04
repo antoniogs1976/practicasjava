097f26b503526d6bd1d3deee66032f3d1af1fc92
Antonio García <antoniogs1976@gmail.com>
2019-12-04 13:59:53 +0100

    Tema3

---
+++ tema3/Circulo.java
@@ -0,0 +1,12 @@
+package tema3;
+
+/**
+ * Circulo
+ * página 26
+ */
+public class Circulo {
+
+    int x;
+    int y;
+    int radio;
+}
---
+++ tema3/UsarCirculo.java
@@ -0,0 +1,14 @@
+package tema3;
+
+/**
+ * UsarCirculo
+ */
+public class UsarCirculo {
+
+    public static void main(String[] args) {
+        Circulo circulo1 = new Circulo(), circulo2 = new Circulo();
+        System.out.print("\033\143");
+        System.out.println("circulo1: " + circulo1);
+        System.out.println("circulo2: " + circulo2);
+    }
+}
