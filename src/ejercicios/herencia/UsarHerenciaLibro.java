060e3cb575f663edede9b70010ee701a4b38ef4b
Antonio García <antoniogs1976@gmail.com>
2019-12-15 22:13:30 +0100

    pruebas domingo

--- src/ejercicios/herencia/UsarHerenciaLibro.java
+++ src/ejercicios/herencia/UsarHerenciaLibro.java
@@ -17,20 +17,43 @@ public class UsarHerenciaLibro {
     public static void main(String[] args) {
         Libro libro1 = new Libro("Halloween, la muerte sale de fiesta", "978-84-17645-06-9", 304);
         LibroInfantil libro2 = new LibroInfantil("La cosa", "978-84-9182-999-7", 32);
+        // Ajustar la edad de libro2
+        libro2.setEdadRecomendada(7);
 
         // limpiar pantalla...
         limpiarPantalla();
         // comprobar si están abiertos o no los libros:
         if (libro1.isAbierto()) {
             escribir("El libro \"" + libro1.getTitulo() + "\" ha sido abierto.");
+            libro1.abrirLibro();
         } else {
             escribir("El libro \"" + libro1.getTitulo() + "\" no ha sido abierto.");            
         }
+        if (libro1.isAbierto()) {
+            escribir("El libro \"" + libro1.getTitulo() + "\" ha sido abierto.");
+        } else {
+            escribir("El libro \"" + libro1.getTitulo() + "\" no ha sido abierto.");            
+        }
+        // ******************************************************************************************
         // libro 2
+        if (libro2.isAbierto()){
+            escribir("El libro \"" + libro2.getTitulo() + "\" ha sido abierto.");
+            libro2.abrirLibro();
+        } else {
+            escribir("El libro \"" + libro2.getTitulo() + "\" no ha sido abierto.");            
+        }
+        escribir("*------------------------------------------------------------*");
         if (libro2.isAbierto()){
             escribir("El libro \"" + libro2.getTitulo() + "\" ha sido abierto.");
         } else {
             escribir("El libro \"" + libro2.getTitulo() + "\" no ha sido abierto.");            
+        }
+        // mostramos la edad recomendada
+        escribir("Edad recomendada: "+libro2.getEdadRecomendada());
+        if (libro2.getEdadRecomendada() < 18){
+            escribir("Libro recomendado para menores.");
+        } else {
+            escribir("Libro no recomendado para menores de 18 años.");
         }
     }
 }
